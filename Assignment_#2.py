b43ff1cae6da8a41ca47df8ba66467425926d738
jeremy-bower77 <43249077+jeremy-bower77@users.noreply.github.com>
2018-10-21 21:36:22 -0400

    Finished the code for Question #1

--- Assignment_#2.py
+++ Assignment_#2.py
@@ -1,13 +1,37 @@
 ########################################################################################################Question 1 Code:
 
 #doc later
-packageWeight = input("Enter the weight of the package: ")
-
+#Variables
+lowLimit = 2
+midLimit = 6
+highLimit = 10
 
 
 #doc later
-#Use this for later
-#print("Shipping charge: ${0:10,.2f}".format())
+packageWeight = eval(input("Enter the weight of the package: "))
+
+#doc later
+if packageWeight >= highLimit:
+    #doc later
+    shippingRate = 4.75
+
+#doc later
+elif packageWeight < highLimit and packageWeight >= midLimit:
+    #doc later
+    shippingRate = 4
+
+#doc later
+elif packageWeight < midLimit and packageWeight >= lowLimit:
+    #doc later
+    shippingRate = 3
+
+#doc later
+elif packageWeight < lowLimit:
+    #doc later
+    shippingRate = 1.5
+
+#doc later
+print("Shipping charge: ${0:4,.2f}".format(shippingRate))
 
 
 
