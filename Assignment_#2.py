7915c5306de8c296c459c0734a3fb40607c8e566
jeremy-bower77 <43249077+jeremy-bower77@users.noreply.github.com>
2018-10-23 22:45:47 -0400

    Created a For loop for entering the scores for Question #2

--- Assignment_#2.py
+++ Assignment_#2.py
@@ -1,39 +1,58 @@
 ########################################################################################################Question 1 Code:
 
-#doc later
-#Variables
-lowLimit = 2
-midLimit = 6
-highLimit = 10
-
-#doc later
-packageWeight = eval(input("Enter the weight of the package: "))
-
-#doc later
-if packageWeight >= highLimit:
-    #doc later
-    shippingRate = 4.75
-
-#doc later
-elif packageWeight < highLimit and packageWeight >= midLimit:
-    #doc later
-    shippingRate = 4
-
-#doc later
-elif packageWeight < midLimit and packageWeight >= lowLimit:
-    #doc later
-    shippingRate = 3
-
-#doc later
-elif packageWeight < lowLimit:
-    #doc later
-    shippingRate = 1.5
-
-#doc later
-print("Shipping charge: ${0:4,.2f}".format(shippingRate))
+# #doc later
+# #Variables
+# lowLimit = 2
+# midLimit = 6
+# highLimit = 10
+#
+# #doc later
+# packageWeight = eval(input("Enter the weight of the package: "))
+#
+# #doc later
+# if packageWeight >= highLimit:
+#     #doc later
+#     shippingRate = 4.75
+#
+# #doc later
+# elif packageWeight < highLimit and packageWeight >= midLimit:
+#     #doc later
+#     shippingRate = 4
+#
+# #doc later
+# elif packageWeight < midLimit and packageWeight >= lowLimit:
+#     #doc later
+#     shippingRate = 3
+#
+# #doc later
+# elif packageWeight < lowLimit:
+#     #doc later
+#     shippingRate = 1.5
+#
+# #doc later
+# print("Shipping charge: ${0:4,.2f}".format(shippingRate))
 
 
 
 ########################################################################################################Question 2 Code:
+
+#doc later
+
+grades = []
+
+#doc later
+for scoreNumber in range(1, 6):
+
+    #doc later
+    grade = float(input("Enter score {0:.0f}: ".format(scoreNumber)))
+    #doc later
+    grades.append(grade)
+
+
+
+
+
+
+
 
 ########################################################################################################Question 3 Code:
