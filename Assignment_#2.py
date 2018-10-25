321fc59ce005acdd300ad6aedfe728d0136bf047
jeremy-bower77 <43249077+jeremy-bower77@users.noreply.github.com>
2018-10-25 14:33:04 -0400

    Created a function that determines letter grades from the scores list in the main function (Question 2)

--- Assignment_#2.py
+++ Assignment_#2.py
@@ -10,22 +10,22 @@
 # packageWeight = eval(input("Enter the weight of the package: "))
 #
 # #doc later
-# if packageWeight >= highLimit:
+# if (packageWeight >= highLimit):
 #     #doc later
 #     shippingRate = 4.75
 #
 # #doc later
-# elif packageWeight < highLimit and packageWeight >= midLimit:
+# elif (packageWeight < highLimit and packageWeight >= midLimit):
 #     #doc later
 #     shippingRate = 4
 #
 # #doc later
-# elif packageWeight < midLimit and packageWeight >= lowLimit:
+# elif (packageWeight < midLimit and packageWeight >= lowLimit):
 #     #doc later
 #     shippingRate = 3
 #
 # #doc later
-# elif packageWeight < lowLimit:
+# elif (packageWeight < lowLimit):
 #     #doc later
 #     shippingRate = 1.5
 #
@@ -38,24 +38,96 @@
 
 #This is the main function
 def main():
+    # doc later
+    scoresList = []
+
     #doc later
     for scoreNumber in range(1, 6):
+        #doc later
+        score = float(input("Enter score {0:.0f}: ".format(scoreNumber)))
+        #doc later
+        scoresList.append(score)
+
+    #doc later
+    firstScore = scoresList[0]
+    secondScore = scoresList[1]
+    thirdScore = scoresList[2]
+    fourthScore = scoresList[3]
+    fifthScore = scoresList[4]
+
+    #doc later
+    print("\nScore\t Numeric Grade\t Letter Grade")
+
+    #doc later
+    ascii()
+
+    #doc later
+    determine_grade(scoresList)
+
+    #doc later
+    ascii()
+
+#doc later
+def determine_grade(scoresList):
+
+    aGradeMaximum = 100
+    aGradeMinimum = 90
+    bGradeMaximum = 89.99
+    bGradeMinimum = 80
+    cGradeMaximum = 79.99
+    cGradeMinimum = 70
+    dGradeMaximum = 69.99
+    dGradeMinimum = 60
+    fGradeMaximum = 59.99
+    fGradeMinimum = 0
+
+    #doc later
+    for score in scoresList:
+
         # doc later
-        grades = []
-        #doc later
-        grade = float(input("Enter score {0:.0f}: ".format(scoreNumber)))
-        #doc later
-        grades.append(grade)
-    print("Score\t Numeric Grade\t Letter Grade")
+        if (score >= aGradeMaximum or score >= aGradeMinimum):
+
+            # doc later
+            letterGrade = "A"
+
+
+        # doc later
+        elif (score <= bGradeMaximum and score >= bGradeMinimum):
+
+            # doc later
+            letterGrade = "B"
+
+
+        # doc later
+        elif (score <= cGradeMaximum and score >= cGradeMinimum):
+
+            # doc later
+            letterGrade = "C"
+
+
+        # doc later
+        elif (score <= dGradeMaximum and score >= dGradeMinimum):
+
+            # doc later
+            letterGrade = "D"
+
+
+        # doc later
+        elif (score <= fGradeMaximum and score >= fGradeMinimum):
+
+            # doc later
+            letterGrade = "F"
+
+
+
+
+#This is some simple ascii design
+def ascii():
+
+    #This is some simple ascii design
+    print("------------------------------------------------------------------")
 
 #This calls the main function
 main()
-
-
-
-
-
-
-
 
 ########################################################################################################Question 3 Code:
