f25174cec949c6e73cd6b55da1e26a189dddbcb0
jeremy-bower77 <43249077+jeremy-bower77@users.noreply.github.com>
2018-11-03 15:09:41 -0400

    Started Question #3 and added the read the text file

--- Assignment_#2.py
+++ Assignment_#2.py
@@ -43,143 +43,177 @@
 
 ########################################################################################################Question 2 Code:
 
-#This is the main function
-def main():
-
-    #This is the score list
-    scoresList = []
-
-    # doc later
-    letterGrades = []
-
-    #doc later
-    for scoreNumber in range(1, 6):
-        #doc later
-        score = float(input("Enter score {0:.0f}: ".format(scoreNumber)))
-        #doc later
-        scoresList.append(score)
-
-    # doc later
-    calc_average(scoresList)
-
-    # doc later
-    determine_grade(scoresList, letterGrades)
-
-    # doc later
-    print("Score\t\t Numeric Grade\t Letter Grade")
-
-    #This outputs a simple ascii design
-    ascii()
-
-    #doc later
-    print("Score 1: {0:10,.1f}".format(scoresList[0]), "\t\t", letterGrades[0])
-
-    # doc later
-    print("Score 2: {0:10,.1f}".format(scoresList[1]), "\t\t", letterGrades[1])
-
-    # doc later
-    print("Score 3: {0:10,.1f}".format(scoresList[2]), "\t\t", letterGrades[2])
-
-    # doc later
-    print("Score 4: {0:10,.1f}".format(scoresList[3]), "\t\t", letterGrades[3])
-
-    # doc later
-    print("Score 5: {0:10,.1f}".format(scoresList[4]), "\t\t", letterGrades[4])
-
-    # This outputs a simple ascii design
-    ascii()
-
-    #doc later
-    print("Average score: {0:2,f}".format(scoresList[5]), "\t", letterGrades[5])
-
-
-#doc later
-def calc_average(scoresList):
-
-    # doc later
-    score = sum(scoresList) / len(scoresList)
-    #doc later
-    scoresList.append(score)
-
-
-#doc later
-def determine_grade(scoresList, letterGrades):
-
-    #doc later
-    aGradeMaximum = 100
-    aGradeMinimum = 90
-    bGradeMaximum = 89.99
-    bGradeMinimum = 80
-    cGradeMaximum = 79.99
-    cGradeMinimum = 70
-    dGradeMaximum = 69.99
-    dGradeMinimum = 60
-    fGradeMaximum = 59.99
-    fGradeMinimum = 0
-
-    #doc later
-    for score in scoresList:
-
-        # doc later
-        if (score >= aGradeMaximum or score >= aGradeMinimum):
-
-            # doc later
-            letterGrade = "A"
-            #doc later
-            letterGrades.append(letterGrade)
-
-        # doc later
-        elif (score <= bGradeMaximum and score >= bGradeMinimum):
-
-            # doc later
-            letterGrade = "B"
-            # doc later
-            letterGrades.append(letterGrade)
-
-        # doc later
-        elif (score <= cGradeMaximum and score >= cGradeMinimum):
-
-            # doc later
-            letterGrade = "C"
-            # doc later
-            letterGrades.append(letterGrade)
-
-        # doc later
-        elif (score <= dGradeMaximum and score >= dGradeMinimum):
-
-            # doc later
-            letterGrade = "D"
-            # doc later
-            letterGrades.append(letterGrade)
-
-        # doc later
-        elif (score <= fGradeMaximum and score >= fGradeMinimum):
-
-            # doc later
-            letterGrade = "F"
-            #doc later
-            letterGrades.append(letterGrade)
-
-        #doc later
-        else:
-
-            #doc later
-            letterGrade = "N/A"
-            #doc later
-            letterGrades.append(letterGrade)
-
-
-
-#This is some simple ascii design
-def ascii():
-
-    #This is some simple ascii design
-    print("-------------------------------------------------")
-
-#This calls the main function
-main()
+# #This is the main function
+# def main():
+#
+#     #This is the score list
+#     scoresList = []
+#
+#     # doc later
+#     letterGrades = []
+#
+#     #doc later
+#     for scoreNumber in range(1, 6):
+#         #doc later
+#         score = float(input("Enter score {0:.0f}: ".format(scoreNumber)))
+#         #doc later
+#         scoresList.append(score)
+#
+#     # doc later
+#     calc_average(scoresList)
+#
+#     # doc later
+#     determine_grade(scoresList, letterGrades)
+#
+#     # doc later
+#     print("Score\t\t Numeric Grade\t Letter Grade")
+#
+#     #This outputs a simple ascii design
+#     ascii()
+#
+#     #doc later
+#     print("Score 1: {0:10,.1f}".format(scoresList[0]), "\t\t", letterGrades[0])
+#
+#     # doc later
+#     print("Score 2: {0:10,.1f}".format(scoresList[1]), "\t\t", letterGrades[1])
+#
+#     # doc later
+#     print("Score 3: {0:10,.1f}".format(scoresList[2]), "\t\t", letterGrades[2])
+#
+#     # doc later
+#     print("Score 4: {0:10,.1f}".format(scoresList[3]), "\t\t", letterGrades[3])
+#
+#     # doc later
+#     print("Score 5: {0:10,.1f}".format(scoresList[4]), "\t\t", letterGrades[4])
+#
+#     # This outputs a simple ascii design
+#     ascii()
+#
+#     #doc later
+#     print("Average score: {0:2,f}".format(scoresList[5]), "\t", letterGrades[5])
+#
+#
+# #doc later
+# def calc_average(scoresList):
+#
+#     # doc later
+#     score = sum(scoresList) / len(scoresList)
+#     #doc later
+#     scoresList.append(score)
+#
+#
+# #doc later
+# def determine_grade(scoresList, letterGrades):
+#
+#     #doc later
+#     aGradeMaximum = 100
+#     aGradeMinimum = 90
+#     bGradeMaximum = 89.99
+#     bGradeMinimum = 80
+#     cGradeMaximum = 79.99
+#     cGradeMinimum = 70
+#     dGradeMaximum = 69.99
+#     dGradeMinimum = 60
+#     fGradeMaximum = 59.99
+#     fGradeMinimum = 0
+#
+#     #doc later
+#     for score in scoresList:
+#
+#         # doc later
+#         if (score >= aGradeMaximum or score >= aGradeMinimum):
+#
+#             # doc later
+#             letterGrade = "A"
+#             #doc later
+#             letterGrades.append(letterGrade)
+#
+#         # doc later
+#         elif (score <= bGradeMaximum and score >= bGradeMinimum):
+#
+#             # doc later
+#             letterGrade = "B"
+#             # doc later
+#             letterGrades.append(letterGrade)
+#
+#         # doc later
+#         elif (score <= cGradeMaximum and score >= cGradeMinimum):
+#
+#             # doc later
+#             letterGrade = "C"
+#             # doc later
+#             letterGrades.append(letterGrade)
+#
+#         # doc later
+#         elif (score <= dGradeMaximum and score >= dGradeMinimum):
+#
+#             # doc later
+#             letterGrade = "D"
+#             # doc later
+#             letterGrades.append(letterGrade)
+#
+#         # doc later
+#         elif (score <= fGradeMaximum and score >= fGradeMinimum):
+#
+#             # doc later
+#             letterGrade = "F"
+#             #doc later
+#             letterGrades.append(letterGrade)
+#
+#         #doc later
+#         else:
+#
+#             #doc later
+#             letterGrade = "N/A"
+#             #doc later
+#             letterGrades.append(letterGrade)
+#
+#
+#
+# #This is some simple ascii design
+# def ascii():
+#
+#     #This is some simple ascii design
+#     print("-------------------------------------------------")
+#
+# #This calls the main function
+# main()
 
 ########################################################################################################Question 3 Code:
 
-# def main():
+#doc later
+def main():
+
+    #doc later
+    originalList = open("Q4-Data.txt", 'r')
+
+    #doc later
+    text_read(originalList)
+
+#doc later
+def text_read(originalList):
+
+   #doc later
+   print([line.rstrip() for line in originalList])
+
+   #doc later
+   originalList.close()
+
+
+# #doc later
+# def list_sorted():
 #
+#     #doc later
+
+# #doc later
+# def new_list_create():
+#
+#     #doc later
+
+# #doc later
+# def list_separate():
+#
+#     #doc later
+
+#doc later
+main()
