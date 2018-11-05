a2e2c34b6bcbd288d8d8a09b229aac7d5bca8f42
jeremy-bower77 <43249077+jeremy-bower77@users.noreply.github.com>
2018-11-04 20:34:09 -0500

    Finished the documentation for questions #1 and #2

--- Assignment_#2.py
+++ Assignment_#2.py
@@ -43,69 +43,76 @@ print("Shipping charge: ${0:4,.2f}".format(shippingRate))
 
 #######################################################################################################Question 2 Code:
 
-#This is the main function
+# This is the main function
 def main():
 
-    #This is the score list
+    # This is the score list
     scoresList = []
 
-    # doc later
+    # This is the score list
     letterGrades = []
 
-    #doc later
+    # This is a for loop that keeps looping the scoreNumber from 1 to 5 until there are 5 scores that are entered
     for scoreNumber in range(1, 6):
-        #doc later
+        # This asks the user to enter in a score and it takes the users input and assigns it to the variable score
         score = float(input("Enter score {0:.0f}: ".format(scoreNumber)))
-        #doc later
+        # This takes the variable score and appends it to the scoresList
         scoresList.append(score)
 
-    # doc later
+    # This is calling the calc_average function and is passing the scoresList to it
     calc_average(scoresList)
 
-    # doc later
+    # This is calling the determine_grade function and is passing the scoresList and the letterGrades to it
     determine_grade(scoresList, letterGrades)
 
-    # doc later
+    # This displays a simple table header starting with Score then Numeric Grade and last Letter Grade
     print("Score\t\t Numeric Grade\t Letter Grade")
 
-    #This outputs a simple ascii design
+    # This outputs a simple ascii design
     ascii()
 
-    #doc later
+    # This line of code outputs the first score from the scoresList and outputs the first grade from the letterGrades
+    # that was assigned for the first score.
     print("Score 1: {0:10,.1f}".format(scoresList[0]), "\t\t", letterGrades[0])
 
-    # doc later
+    # This line of code outputs the second score from the scoresList and outputs the second grade from the letterGrades
+    # that was assigned for the second score.
     print("Score 2: {0:10,.1f}".format(scoresList[1]), "\t\t", letterGrades[1])
 
-    # doc later
+    # This line of code outputs the third score from the scoresList and outputs the third grade from the letterGrades
+    # that was assigned for the third score.
     print("Score 3: {0:10,.1f}".format(scoresList[2]), "\t\t", letterGrades[2])
 
-    # doc later
+    # This line of code outputs the fourth score from the scoresList and outputs the fourth grade from the letterGrades
+    # that was assigned for the fourth score.
     print("Score 4: {0:10,.1f}".format(scoresList[3]), "\t\t", letterGrades[3])
 
-    # doc later
+    # This line of code outputs the fifth score from the scoresList and outputs the fifth grade from the letterGrades
+    # that was assigned for the fifth score.
     print("Score 5: {0:10,.1f}".format(scoresList[4]), "\t\t", letterGrades[4])
 
     # This outputs a simple ascii design
     ascii()
 
-    #doc later
+    # This line of code outputs the average score for the scoresList and outputs the grade from the letterGrades
+    # that the average score has.
     print("Average score: {0:2,f}".format(scoresList[5]), "\t", letterGrades[5])
 
 
-#doc later
+# This is the calc_average that calls the scoresList from the main and calculates the average of the scores
 def calc_average(scoresList):
 
-    # doc later
+    # This formula calculates the average score from the scores list
     score = sum(scoresList) / len(scoresList)
-    #doc later
+    # This appends the average score to the scoresList
     scoresList.append(score)
 
 
-#doc later
+# This is the determine_grade that calls the scoresList and letterGrades list from the main and assigns a letter grade
+# for each of the scores entered.
 def determine_grade(scoresList, letterGrades):
 
-    #doc later
+    # These are the variables that are used for the if elif statements for assigning the letter grades to the scores
     aGradeMaximum = 100
     aGradeMinimum = 90
     bGradeMaximum = 89.99
@@ -117,55 +124,60 @@ def determine_grade(scoresList, letterGrades):
     fGradeMaximum = 59.99
     fGradeMinimum = 0
 
-    #doc later
+    # This is a for loop that loops for each score in the scoresList
     for score in scoresList:
 
-        # doc later
+        # This is an if statement that checks to see if the score higher or equal to the aGradeMaximum or if the score
+        # higher or equal to the aGradeMinimum.
         if (score >= aGradeMaximum or score >= aGradeMinimum):
 
-            # doc later
+            # This assigns the letterGrade to A
             letterGrade = "A"
-            #doc later
+            # This appends the letterGrade to the letterGrades list
             letterGrades.append(letterGrade)
 
-        # doc later
+        # This is an elif statement that checks to see if the score lower or equal to the bGradeMaximum and if the score
+        # higher or equal to the bGradeMinimum.
         elif (score <= bGradeMaximum and score >= bGradeMinimum):
 
-            # doc later
+            # This assigns the letterGrade to B
             letterGrade = "B"
-            # doc later
+            # This appends the letterGrade to the letterGrades list
             letterGrades.append(letterGrade)
 
-        # doc later
+        # This is an elif statement that checks to see if the score lower or equal to the cGradeMaximum and if the score
+        # higher or equal to the cGradeMinimum.
         elif (score <= cGradeMaximum and score >= cGradeMinimum):
 
-            # doc later
+            # This assigns the letterGrade to C
             letterGrade = "C"
-            # doc later
+            # This appends the letterGrade to the letterGrades list
             letterGrades.append(letterGrade)
 
-        # doc later
+        # This is an elif statement that checks to see if the score lower or equal to the dGradeMaximum and if the score
+        # higher or equal to the dGradeMinimum.
         elif (score <= dGradeMaximum and score >= dGradeMinimum):
 
-            # doc later
+            # This assigns the letterGrade to D
             letterGrade = "D"
-            # doc later
+            # This appends the letterGrade to the letterGrades list
             letterGrades.append(letterGrade)
 
-        # doc later
+        # This is an elif statement that checks to see if the score lower or equal to the fGradeMaximum and if the score
+        # higher or equal to the fGradeMinimum.
         elif (score <= fGradeMaximum and score >= fGradeMinimum):
 
-            # doc later
+            # This assigns the letterGrade to F
             letterGrade = "F"
-            #doc later
+            # This appends the letterGrade to the letterGrades list
             letterGrades.append(letterGrade)
 
-        #doc later
+        # This is an else statement that runs if the none of the other if and elif statements are true.
         else:
 
-            #doc later
-            letterGrade = "N/A"
-            #doc later
+            # This assigns the letterGrade to E
+            letterGrade = "E"
+            # This appends the letterGrade to the letterGrades list
             letterGrades.append(letterGrade)
 
 
